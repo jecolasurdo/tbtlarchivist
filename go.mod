342dcf4b0584cb0ea1239365bd20aec6ccab2afe
Joe <jecolasurdo@gmail.com>
2021-01-27 18:54:29 -0800

    Rewrote mg scraper without chrome.

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/jecolasurdo/tbtlarchivist
 go 1.15
 
 require (
+	github.com/antchfx/htmlquery v1.2.3
+	github.com/antchfx/xpath v1.1.11
 	github.com/chromedp/chromedp v0.6.5
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 )
--- services/curators/clipcurators/marsupialgurgle/cmd/main.go
+++ services/curators/clipcurators/marsupialgurgle/cmd/main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"regexp"
 	"strconv"
-	"sync"
+	"time"
 
-	"github.com/chromedp/chromedp"
-	"github.com/jecolasurdo/tbtlarchivist/pacer"
-	"github.com/jecolasurdo/tbtlarchivist/services/curators/internal/cdp"
+	"github.com/antchfx/htmlquery"
+	"github.com/antchfx/xpath"
 	"github.com/jecolasurdo/tbtlarchivist/services/curators/internal/utils"
 )
 
 const (
 	marsupialgurgleBaseURI = `https://marsupialgurgle.com`
 
+	pageCountXPath = `//*[@id="bottom-nav-pagination"]/a[3]`
+
 	rawMP3LinkRegex         = `href="((?:/[\w+|-]+)+\.mp3)"`
 	mp3WithDescriptionRegex = `(?sU)<p><\w+>(.*)</\w+></p>.*href="((?:/[\w+|-]+)+\.mp3)"`
 )
 
 var (
+	pageCountXp = xpath.MustCompile(pageCountXPath)
+
 	rawMP3LinkRe         = regexp.MustCompile(rawMP3LinkRegex)
 	mp3WithDescriptionRe = regexp.MustCompile(mp3WithDescriptionRegex)
 )
@@ -35,90 +39,70 @@ var (
 //    - Build contextualized lists of mp3s
 //    - Remove any duplicates (same file name more than once)
 func main() {
-	workQueue := new(pacer.Queue)
 
-	_ = workQueue.Poll(context.Background())
-	wg := new(sync.WaitGroup)
-
-	log.Println("Starting Chrome...")
-	ctx, cancel := chromedp.NewContext(
-		context.Background(),
-		chromedp.WithLogf(log.Printf),
-	)
-	defer cancel()
-
-	var rawPageCount string
-	err := chromedp.Run(ctx,
-		cdp.Logf("Navigating to global search results page..."),
-		chromedp.Navigate(`https://www.marsupialgurgle.com/page/1/?s`),
-		cdp.Logf("Getting page count..."),
-		chromedp.Text(`#bottom-nav-pagination > a:nth-child(5)`, &rawPageCount, chromedp.ByQuery),
-	)
+	log.Printf("Navigating to global search results page...")
+	doc, err := htmlquery.LoadURL(`https://www.marsupialgurgle.com/page/1/?s`)
 	utils.LogFatalIfErr(err)
 
+	log.Printf("Getting page count...")
+	rawPageCount := htmlquery.QuerySelector(doc, pageCountXp).FirstChild.Data
 	pageCount, err := strconv.Atoi(rawPageCount)
 	utils.LogFatalIfErr(err)
+	log.Println(pageCount)
 
 	fmt.Println("Temporarily starting after page 1 for testing")
 	for pageNumber := 113; pageNumber <= pageCount; pageNumber++ {
-		wg.Add(1)
-		workQueue.Enqueue(func() error {
-			defer wg.Done()
+		log.Printf("Scraping page %v of %v...", pageNumber, pageCount)
+		resp, err := http.Get(fmt.Sprintf("https://www.marsupialgurgle.com/page/%v/?s", pageNumber))
+		utils.LogFatalIfErr(err)
+		if resp.StatusCode != 200 {
+			log.Fatal(resp.Status)
+		}
 
-			log.Printf("Scraping page %v of %v...", pageNumber, pageCount)
-			var searchDOM string
-			err := chromedp.Run(ctx,
-				chromedp.Navigate(fmt.Sprintf("https://www.marsupialgurgle.com/page/%v/?s", pageNumber)),
-				chromedp.InnerHTML(".search", &searchDOM, chromedp.ByQuery, chromedp.NodeReady),
-			)
-			if err != nil {
-				return err
+		searchHTMLBytes, err := ioutil.ReadAll(resp.Body)
+		utils.LogFatalIfErr(err)
+		searchHTML := string(searchHTMLBytes)
+
+		distinctMP3URIs := map[string]struct{}{}
+		rawMP3Matches := rawMP3LinkRe.FindAllStringSubmatch(searchHTML, -1)
+		for i := 0; i < len(rawMP3Matches); i++ {
+			if len(rawMP3Matches[i]) != 2 {
+				log.Println(rawMP3Matches[i])
+				continue
 			}
+			distinctMP3URIs[rawMP3Matches[i][1]] = struct{}{}
+		}
+		log.Printf("\tDistinct raw mp3 links: %v", len(distinctMP3URIs))
 
-			distinctMP3URIs := map[string]struct{}{}
-			rawMP3Matches := rawMP3LinkRe.FindAllStringSubmatch(searchDOM, -1)
-			for i := 0; i < len(rawMP3Matches); i++ {
-				if len(rawMP3Matches[i]) != 2 {
-					log.Println(rawMP3Matches[i])
-					continue
-				}
-				distinctMP3URIs[rawMP3Matches[i][1]] = struct{}{}
+		decoratedMP3Matches := mp3WithDescriptionRe.FindAllStringSubmatch(searchHTML, -1)
+		distinctDecoratedMP3URIs := map[string]struct{}{}
+		for i := 0; i < len(decoratedMP3Matches); i++ {
+			if len(decoratedMP3Matches[i]) != 3 {
+				log.Println(decoratedMP3Matches[i])
+				continue
 			}
-			log.Printf("\tDistinct raw mp3 links: %v", len(distinctMP3URIs))
+			_ = decoratedMP3Matches[i][1] // description to be used later
+			mp3URI := decoratedMP3Matches[i][2]
+			distinctDecoratedMP3URIs[mp3URI] = struct{}{}
+		}
+		log.Printf("\tDistinct decorated mp3s links: %v", len(distinctDecoratedMP3URIs))
 
-			decoratedMP3Matches := mp3WithDescriptionRe.FindAllStringSubmatch(searchDOM, -1)
-			distinctDecoratedMP3URIs := map[string]struct{}{}
-			for i := 0; i < len(decoratedMP3Matches); i++ {
-				if len(decoratedMP3Matches[i]) != 3 {
-					log.Println(decoratedMP3Matches[i])
-					continue
-				}
-				_ = decoratedMP3Matches[i][1] // description to be used later
-				mp3URI := decoratedMP3Matches[i][2]
-				distinctDecoratedMP3URIs[mp3URI] = struct{}{}
-			}
-			log.Printf("\tDistinct decorated mp3s links: %v", len(distinctDecoratedMP3URIs))
+		if len(distinctDecoratedMP3URIs) != len(distinctMP3URIs) {
 
-			if len(distinctDecoratedMP3URIs) != len(distinctMP3URIs) {
+			// pages 83 and 112 contain a variant in the html formatting which is
+			// <p><strong><br><a href...
+			// whereas the following is more typical for the site
+			// <p><strong> ... </p></strong><p><a href...
+			// Page 112 example /audio/lukeandrewdoyouneedsomealcohol-2748.mp3
+			log.Printf("Mismatch on page %v", pageNumber)
 
-				// pages 83 and 112 contain a variant in the html formatting which is
-				// <p><strong><br><a href...
-				// whereas the following is more typical for the site
-				// <p><strong> ... </p></strong><p><a href...
-				// Page 112 example /audio/lukeandrewdoyouneedsomealcohol-2748.mp3
-				log.Printf("Mismatch on page %v", pageNumber)
-
-				for m := range distinctMP3URIs {
-					if _, found := distinctDecoratedMP3URIs[m]; !found {
-						fmt.Println(m)
-					}
+			for m := range distinctMP3URIs {
+				if _, found := distinctDecoratedMP3URIs[m]; !found {
+					fmt.Println(m)
 				}
 			}
+		}
 
-			return nil
-		})
-
+		time.Sleep(1 * time.Second)
 	}
-
-	wg.Wait()
 }
