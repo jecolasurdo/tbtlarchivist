22c9cfcc234b0377e03407de5d4f7cc9dc6f8801
Joe <jecolasurdo@gmail.com>
2021-01-30 19:31:08 -0800

    WIP: Got too fancy with dynamic channel selection. Rolling back.

--- pkg/archivist/archivist.go
+++ pkg/archivist/archivist.go
@@ -1,13 +1,24 @@
 package archivist
 
-import "github.com/jecolasurdo/tbtlarchivist/pkg/contracts"
+import (
+	"sync"
+
+	"github.com/jecolasurdo/tbtlarchivist/pkg/contracts"
+)
 
 // API is an instance of an archivist. An archivist is responsible for the
 // following tasks:
 //  - Recording episode and clip metadata as reported from the curators.
 //  - Creating work for researchers.
 //  - Checking in work returned from the researchers.
-type API struct{}
+type API struct {
+	ErrorSource <-chan error
+}
+
+type worker struct {
+	source   <-chan interface{}
+	delegate func(interface{}) error
+}
 
 // event loop
 // 	check for curated episode
@@ -15,47 +26,44 @@ type API struct{}
 //	check if there's work to be done for the researchers
 //	check for work returned from researchers
 
-func Start() error {
-	api := new(API)
-	errorTrap := make(chan error)
-
-	curatedEpisodeSource, errorSource := api.getCuratedEpisodeSource()
-	curatedClipSource, errorSource := api.getCuratedClipSource()
-	pendingResearchSource, errorSource := api.getPendingResearchSource()
-	completedResearchSource, errorSource := api.getCompletedResearchSource()
-
-	for {
-		select {
-
-		//TODO: Set Qos for channels to control how much work is buffered for
-		// each consumer instance.
-
-		case curatedEpisode := <-curatedEpisodeSource:
-			err := api.processCuratedEpisode(curatedEpisode)
-			if err != nil {
-				return err
-			}
-		case curatedClip := <-curatedClipSource:
-			err := api.processCuratedClip(curatedClip)
-			if err != nil {
-				return err
-			}
-		case pendingResearch := <-pendingResearchSource:
-			err := processPendingResearch(pendingResearch)
-			if err != nil {
-				return err
-			}
-		case completedResearch := <-completedResearchSource:
-			err := processCompletedResearch(completedResearch)
-			if err != nil {
-				return err
-			}
-		}
-	}
+func Initialize() (*API, error) {
+	workerSource := []<-chan worker{}
+	getCuratedEpisodeSource()
 
 }
 
-func (a *API) getCuratedEpisodeSource() (<-chan contracts.EpisodeInfo, <-chan error) {
+func (a *API) poll(workerSource []<-chan worker) <-chan error {
+	backlog := make(chan worker)
+	go func() {
+		defer close(backlog)
+		wg := new(sync.WaitGroup)
+		for _, w := range workerSource {
+			wg.Add(1)
+			go func(w <-chan worker) {
+				for workItem := range w {
+					backlog <- workItem
+				}
+				wg.Done()
+			}(w)
+		}
+		wg.Wait()
+	}()
+
+	errorSource := make(chan error)
+	go func() {
+		defer close(errorSource)
+		for work := range backlog {
+			errorSource <- work.delegate(work.source)
+		}
+	}()
+
+	return errorSource
+}
+
+//TODO: Set Qos for channels to control how much work is buffered for
+// each consumer instance.
+
+func (a *API) getCuratedEpisodeSource() (<-chan contracts.EpisodeInfo, error) {
 	// episodes are unique by name + date aired
 	// check to see if the episode exists
 	//	if it does not: add it
@@ -70,7 +78,7 @@ func (a *API) processCuratedEpisode(episode contracts.EpisodeInfo) error {
 	panic("not implemented")
 }
 
-func (a *API) getCuratedClipSource() (<-chan contracts.ClipInfo, <-chan error) {
+func (a *API) getCuratedClipSource() (<-chan contracts.ClipInfo, error) {
 	// similar process to episode handling, except clips are unique by name only
 	panic("not implemented")
 }
@@ -79,7 +87,7 @@ func (a *API) processCuratedClip(clip contracts.ClipInfo) error {
 	panic("not implemented")
 }
 
-func (a *API) getPendingResearchSource() (<-chan contracts.ResearchPending, <-chan error) {
+func (a *API) getPendingResearchSource() (<-chan contracts.ResearchPending, error) {
 	// check to see how many consumers there are for a queue
 	// compare the consumer count to the message count
 	// Then determine how much work to create, ie consumerCount - messageCount
@@ -91,7 +99,7 @@ func processPendingResearch(pendingResearch contracts.ResearchPending) error {
 	panic("not implemented")
 }
 
-func (a *API) getCompletedResearchSource() (<-chan contracts.ResearchComplete, <-chan error) {
+func (a *API) getCompletedResearchSource() (<-chan contracts.ResearchComplete, error) {
 	// upsert research and update leases if applicable
 	panic("not implemented")
 }
