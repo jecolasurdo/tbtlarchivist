1c615340650fb206d42598142dee494eb94519fb
Joe <jecolasurdo@gmail.com>
2021-02-03 19:04:43 -0800

    Added researchers doc file.

---
+++ pkg/researchers/doc.go
@@ -0,0 +1,8 @@
+// Package researchers contains libraries that search for the presense of short
+// audio clips within episodes, then notate the location of those clips within
+// each episode.
+//
+// The tbtldrops researcher is written in Rust, and is thus excluded from go's
+// documentation. However this doc file is included so the existance of the
+// researchers package is reflected in the documentation.
+package researchers
