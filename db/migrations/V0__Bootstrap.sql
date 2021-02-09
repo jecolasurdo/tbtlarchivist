9d1f800501e76ec45874fbc677333e5c8aca6919
Joe <jecolasurdo@gmail.com>
2021-02-08 19:19:37 -0800

    add episode_info table to schema.

--- db/migrations/V0__Bootstrap.sql
+++ db/migrations/V0__Bootstrap.sql
@@ -9,4 +9,19 @@ CREATE TABLE `clip_info` (
   PRIMARY KEY (`clip_id`),
   UNIQUE KEY `title_UNIQUE` (`title`),
   UNIQUE KEY `media_uri_UNIQUE` (`media_uri`) USING HASH
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
+
+CREATE TABLE `episode_info` (
+  `episode_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_curated` datetime NOT NULL,
+  `curator_info` varchar(50) NOT NULL,
+  `date_aired` datetime NOT NULL,
+  `duration` int(11),
+  `title` varchar(250),
+  `description` longtext NOT NULL,
+  `media_uri` varchar(2048) NOT NULL,
+  `media_type` varchar(20) NOT NULL,
+  PRIMARY KEY (`episode_id`),
+  UNIQUE KEY `date_aired_title_UNIQUE` (`date_aired`, `title`),
+  UNIQUE KEY `media_uri_UNIQUE` (`media_uri`) USING HASH
+);
