7a527c84820ad11f2b3089e8a6c3cdc0705d06e9
Joe <jecolasurdo@gmail.com>
2021-02-13 20:58:45 -0800

    Implemented GetHighestPriorityEpisode

--- pkg/accessors/datastore/adapters/mariadbadapter/research.go
+++ pkg/accessors/datastore/adapters/mariadbadapter/research.go
@@ -1,12 +1,59 @@
 package mariadbadapter
 
-import "github.com/jecolasurdo/tbtlarchivist/pkg/contracts"
+import (
+	"database/sql"
+
+	"github.com/jecolasurdo/tbtlarchivist/pkg/contracts"
+)
 
 // GetHighestPriorityEpisode identifies and returns the highest priority
 // episode to be researched. If no episodes are available, this returns nil,
 // nil.
 func (m *MariaDbConnection) GetHighestPriorityEpisode() (*contracts.EpisodeInfo, error) {
-	panic("not implemented")
+	selectStmt := `
+		SELECT 
+			ce.initial_date_curated,
+			ce.last_date_curated,
+			ce.curator_info,
+			ce.date_aired,
+			ce.title,
+			ce.description,
+			ce.media_uri,
+			ce.media_type,
+			ce.priority
+		FROM 
+			research_backlog rb
+			LEFT JOIN research_leases rl ON rb.research_id != rl.research_id
+			JOIN curated_episodes ce ON rb.episode_id = ce.episode_id
+		ORDER BY
+			ce.priority DESC,
+			cd.last_date_curated DESC
+		LIMIT 1;
+	`
+
+	row := m.db.QueryRow(selectStmt)
+	episodeInfo := new(contracts.EpisodeInfo)
+	err := row.Scan(
+		&episodeInfo.InitialDateCurated,
+		&episodeInfo.LastDateCurated,
+		&episodeInfo.CuratorInformation,
+		&episodeInfo.DateAired,
+		&episodeInfo.Title,
+		&episodeInfo.Description,
+		&episodeInfo.MediaURI,
+		&episodeInfo.MediaType,
+		&episodeInfo.Priority,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return episodeInfo, nil
 }
 
 // GetHighestPriorityClipsForEpisode identifies and returns the highest
