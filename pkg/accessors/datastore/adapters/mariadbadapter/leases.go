db89380da4e4da718f1a39b7cb9de8ef58925a6b
Joe <jecolasurdo@gmail.com>
2021-02-14 00:59:43 -0800

    WIP: still working on CreateResearchLease implementation

--- pkg/accessors/datastore/adapters/mariadbadapter/leases.go
+++ pkg/accessors/datastore/adapters/mariadbadapter/leases.go
@@ -2,7 +2,6 @@ package mariadbadapter
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/jecolasurdo/tbtlarchivist/pkg/contracts"
@@ -17,7 +16,7 @@ import (
 // method will panic if clips is empty or nil. If there are no clips to lease
 // for an episode, that should be handled without attempting to call this
 // method.
-func (m *MariaDbConnection) CreateResearchLease(newLeaseID string, episode contracts.EpisodeInfo, clips []contracts.ClipInfo, deadline time.Time) error {
+func (m *MariaDbConnection) CreateResearchLease(newLeaseID string, episode contracts.EpisodeInfo, clips []contracts.ClipInfo, expiration time.Time) error {
 	if len(clips) == 0 {
 		panic("a non-zero number of clips must be supplied to this method")
 	}
@@ -30,54 +29,31 @@ func (m *MariaDbConnection) CreateResearchLease(newLeaseID string, episode contr
 		return fmt.Errorf("episode not found in database %v", episode)
 	}
 
-	panic("TODO (see comment below)")
-	// The way you're doing this is dumb.
-	// The title concatenation is not terrible, but from there you should just
-	// do some joins to idenfity the leasable set.
-	// Let the database do the work.
-
-	// verify each clip exists by getting their ids
-	titles := make([]string, len(clips))
-	for _, clip := range clips {
-		titles = append(titles, fmt.Sprintf(`"%v"`, clip.Title))
-	}
-
-	const selectClipIDsStmt = `
-		SELECT clip_id FROM curated_clips WHERE title IN [%v]
-	`
-	preparedInsertClipIDsStmt := fmt.Sprintf(selectClipIDsStmt, strings.Join(titles, ","))
-
 	tx, err := m.db.Begin()
 	if err != nil {
 		return err
 	}
 
-	rows, err := tx.Query(preparedInsertClipIDsStmt)
-	if err != nil {
-		return tryTxRollback(tx, err)
+	const insertStmt = `
+		INSERT INTO research_leases (lease_id, research_id, expiration)
+			SELECT "%v", bl.research_id, "%v" 
+			FROM research_backlog bl
+				JOIN curated_clips cc ON bl.clip_id = cc.clip_id
+			WHERE cc.title = ?
+	`
+	// Preparing the statement with newLeaseID and expiration here, but leaving
+	// the title value to be prepared by the sql package. Placeholders can't be
+	// used in the select clause, but we prefer to rely on the sql engine to
+	// escape/convert datatypes as much as possible.
+	partiallyPreparedInsert := fmt.Sprintf(insertStmt, newLeaseID, expiration)
+
+	for _, clip := range clips {
+		panic("TODO (see below)")
+		// range over the clips
+		// prepare each insert statement
+		// if the insert fails, or alters 0 records rollback and return an error
 	}
 
-	clipIDs := make([]int, len(clips))
-	for rows.Next() {
-		err := rows.Err()
-		if err != nil {
-			return tryTxRollback(tx, err)
-		}
-		var clipID int
-		err = rows.Scan(&clipID)
-		if err != nil {
-			return tryTxRollback(tx, err)
-		}
-		clipIDs = append(clipIDs, clipID)
-	}
-
-	if len(clipIDs) != len(clips) {
-		err = fmt.Errorf("requested IDs for %v clips, but found IDs for %v clips", len(clips), len(clipIDs))
-		return tryTxRollback(tx, err)
-	}
-
-	// verify that none of them have an active lease
-	// insert a lease item for each clip/episode pair
 	panic("not implemented")
 }
 
