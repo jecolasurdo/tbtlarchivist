b439eb4427bf7c7bca2e12ebc82bdfe77fd8944f
Joe <jecolasurdo@gmail.com>
2021-02-14 19:23:32 -0800

    mariadb injected into pendingresearcharchivist

--- services/archivists/pendingresearcharchivist/main.go
+++ services/archivists/pendingresearcharchivist/main.go
@@ -1,33 +1,47 @@
 package main
 
-// import (
-// 	"context"
-// 	"log"
+import (
+	"context"
+	"log"
+	"time"
 
-// 	"github.com/jecolasurdo/tbtlarchivist/pkg/accessors/datastore/adapters/fakedatastore"
-// 	"github.com/jecolasurdo/tbtlarchivist/pkg/accessors/messagebus/adapters/amqpadapter"
-// 	"github.com/jecolasurdo/tbtlarchivist/pkg/archivists"
-// )
+	"github.com/jecolasurdo/tbtlarchivist/pkg/accessors/datastore/adapters/mariadbadapter"
+	"github.com/jecolasurdo/tbtlarchivist/pkg/accessors/messagebus/adapters/amqpadapter"
+	"github.com/jecolasurdo/tbtlarchivist/pkg/archivists"
+)
 
-// func main() {
+func main() {
+	log.Println("Connecting to database...")
+	dbconfig := &mariadbadapter.Config{
+		Addr:                  "127.0.0.1:3306",
+		DBName:                "tbtlarchivist",
+		User:                  "root",
+		MaxConnectionLifetime: 60 * time.Second,
+		MaxOpenConnections:    5,
+		MaxIdleConnections:    5,
+	}
+	db, err := mariadbadapter.New(dbconfig).Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	log.Println("Connecting to message bus...")
-// 	msgbus, err := amqpadapter.Initialize(context.Background(), "pending_research", 5)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	log.Println("Connecting to message bus...")
+	msgbus, err := amqpadapter.Initialize(context.Background(), "pending_research", 5)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	log.Println("Starting pending-research archivist...")
-// 	pendingResearchArchivist := archivists.StartPendingResearchArchivist(context.Background(), msgbus, new(fakedatastore.FakeDataStorer))
+	log.Println("Starting pending-research archivist...")
+	pendingResearchArchivist := archivists.StartPendingResearchArchivist(context.Background(), msgbus, db)
 
-// 	log.Println("Running...")
-// 	for {
-// 		select {
-// 		case err := <-pendingResearchArchivist.Errors:
-// 			log.Println(err)
-// 		case <-pendingResearchArchivist.Done:
-// 			log.Println("Done")
-// 			return
-// 		}
-// 	}
-// }
+	log.Println("Running...")
+	for {
+		select {
+		case err := <-pendingResearchArchivist.Errors:
+			log.Println(err)
+		case <-pendingResearchArchivist.Done:
+			log.Println("Done")
+			return
+		}
+	}
+}
