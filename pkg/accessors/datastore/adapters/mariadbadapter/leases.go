797f35ac4cc06b3d206a8a9a5d21fd70a39c9ad1
Joe <jecolasurdo@gmail.com>
2021-02-14 19:16:29 -0800

    RenewResearchLease and RevokeResearchLease implemented

--- pkg/accessors/datastore/adapters/mariadbadapter/leases.go
+++ pkg/accessors/datastore/adapters/mariadbadapter/leases.go
@@ -9,14 +9,9 @@ import (
 )
 
 // CreateResearchLease attempts to create a lease that is shared between the
-// episode and all provided clips. This operation is atomic. If the episode or
-// any of the provided clips either a) doesn't exist or b) already has an
-// active lease, this operation will fail and return an error. This method also
-// presumes the supplied `newLeaseID` value is sufficiently unique (i.e.  a
-// UUID), and will return an error if the supplied ID is already in use. This
-// method will panic if clips is empty or nil. If there are no clips to lease
-// for an episode, that should be handled without attempting to call this
-// method.
+// episode and all provided clips.  This method will panic if clips is empty or
+// nil. If there are no clips to lease for an episode, that should be handled
+// without attempting to call this method.
 func (m *MariaDbConnection) CreateResearchLease(newLeaseID uuid.UUID, episode contracts.EpisodeInfo, clips []contracts.ClipInfo, expiration time.Time) error {
 	if len(clips) == 0 {
 		panic("a non-zero number of clips must be supplied to this method")
@@ -63,12 +58,27 @@ func (m *MariaDbConnection) CreateResearchLease(newLeaseID uuid.UUID, episode co
 
 // RenewResearchLease updates the deadline for an existing lease. If the lease
 // doesn't exist, no action is taken.
-func (m *MariaDbConnection) RenewResearchLease(leaseID uuid.UUID, deadline time.Time) error {
-	panic("not implemented")
+func (m *MariaDbConnection) RenewResearchLease(leaseID uuid.UUID, expiration time.Time) error {
+	const selectStmt = `
+		UPDATE research_leases
+		SET expiration = ?
+		WHERE lease_id = ?
+	`
+	// We ignore the returned SQLResult value since we're not concerned with
+	// how many (if any) leases were renewed.
+	_, err := m.db.Exec(selectStmt, expiration, leaseID)
+	return err
 }
 
 // RevokeResearchLease removes the leases for all items assigned to the
 // specified leaseID. If the leaseID doesn't exist, no action is taken.
 func (m *MariaDbConnection) RevokeResearchLease(leaseID uuid.UUID) error {
-	panic("not implemented")
+	const deleteStmt = `
+		DELETE FROM research_leases
+		WHERE lease_id = ?
+	`
+	// We ignore the returned SQLResult value since we're not concerned with
+	// how many (if any) leases were revoked.
+	_, err := m.db.Exec(deleteStmt, leaseID)
+	return err
 }
