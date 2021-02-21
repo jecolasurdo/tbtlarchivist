543c4adb95a9d34779f29ae798a7547585494bbc
Joe <jecolasurdo@gmail.com>
2021-02-20 16:29:08 -0800

    Pairing back analyst cli to just return whatever it is sent

--- rust/analyst/cli/src/main.rs
+++ rust/analyst/cli/src/main.rs
@@ -1,28 +1,32 @@
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-use analyzer::{analyze, read_wav};
+// use analyzer::{analyze, read_wav};
 use anyhow::Result;
+use std::io::{self, Read};
 
-const FILE_NAME_EPISODE: &str = "/Users/Joe/Documents/code/tbtldrops/audio/episodes/episode.wav";
-const FILE_NAME_DROP: &str = "/Users/Joe/Documents/code/tbtldrops/audio/drops/drop.wav";
+// const FILE_NAME_EPISODE: &str = "/Users/Joe/Documents/code/tbtldrops/audio/episodes/episode.wav";
+// const FILE_NAME_DROP: &str = "/Users/Joe/Documents/code/tbtldrops/audio/drops/drop.wav";
 
 fn main() -> Result<()> {
-    let episode = read_wav(FILE_NAME_EPISODE.to_owned())?;
-    println!("episode samples: {}", episode.len());
+    // let episode = read_wav(FILE_NAME_EPISODE.to_owned())?;
+    // println!("episode samples: {}", episode.len());
 
-    let drop = read_wav(FILE_NAME_DROP.to_owned())?;
-    println!("{}", drop.len());
+    // let drop = read_wav(FILE_NAME_DROP.to_owned())?;
+    // println!("{}", drop.len());
 
-    let analyzer_options = analyzer::AnalyzerOptions {
-        pass_one_sample_density: 1,
-        pass_one_sample_size: 9,
-        pass_one_threshold: 0.991,
-        pass_two_sample_size: 50,
-        pass_two_threshold: 0.99,
-    };
+    // let analyzer_options = analyzer::AnalyzerOptions {
+    //     pass_one_sample_density: 1,
+    //     pass_one_sample_size: 9,
+    //     pass_one_threshold: 0.991,
+    //     pass_two_sample_size: 50,
+    //     pass_two_threshold: 0.99,
+    // };
 
-    analyze(episode, drop, analyzer_options)?;
-
+    // analyze(episode, drop, analyzer_options)?;
+    
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    print!("hello {}", buffer);
     Ok(())
 }
