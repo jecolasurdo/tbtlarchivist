2009fd1278fbc7a1b8a91f74ea67855281c03427
Joe <jecolasurdo@gmail.com>
2021-02-20 16:46:07 -0800

    enforce Analyzer for rustanalyst

--- go/internal/accessors/analyst/adapters/rustanalyst/pkg.go
+++ go/internal/accessors/analyst/adapters/rustanalyst/pkg.go
@@ -1,5 +1,9 @@
 package rustanalyst
 
+import "github.com/jecolasurdo/tbtlarchivist/go/internal/accessors/analyst"
+
 // The Adapter spawns a child analyst-rust process, and marshals messages between
 // the caller and the child processes.
 type Adapter struct{}
+
+var _ analyst.Analyzer = (*Adapter)(nil)
