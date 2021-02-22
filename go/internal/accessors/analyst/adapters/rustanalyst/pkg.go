1108fac81062083e73ec09e62cc8f34b3adda725
Joe <jecolasurdo@gmail.com>
2021-02-21 18:48:48 -0800

    Implemented ScanFrames within Run

--- go/internal/accessors/analyst/adapters/rustanalyst/pkg.go
+++ go/internal/accessors/analyst/adapters/rustanalyst/pkg.go
@@ -3,27 +3,57 @@ package rustanalyst
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"os/exec"
 
 	"github.com/jecolasurdo/tbtlarchivist/go/internal/accessors/analyst"
 	"github.com/jecolasurdo/tbtlarchivist/go/internal/contracts"
+	"github.com/jecolasurdo/tbtlarchivist/go/internal/utils"
 	"google.golang.org/protobuf/proto"
 )
 
-// The Adapter spawns a child analyst-rust process, and marshals messages between
-// the caller and the child processes.
-type Adapter struct{}
+// The Adapter spawns a child analyst-rust process, and marshals messages
+// between the caller and the child process.
+type Adapter struct {
+
+	// PathResolver is a function that returns the path to the analyst process
+	// to be spawned. If this value is nil, DefaultPathResolver is used.
+	PathResolver func() (string, error)
+}
+
+// DefaultPathResolver is used to locate an "analyzer" binary in the local
+// PATH.
+func DefaultPathResolver() (string, error) {
+	path, err := exec.LookPath("analyzer")
+	if err != nil {
+		return "", fmt.Errorf("error occured while locating the analyzer\n%v", err)
+	}
+	return path, nil
+}
 
 // Run starts a rust analyst as a child process, pipes pendingResearch to the
-// process via stdin, and listens for results on stdout.
+// process via stdin, and listens for results on stdout. The returned
+// CompletedResearchItem and error channels will remain open until all work is
+// completed, at which time they are both closed.  Any errors that occur before
+// the adapter begins polling stdout will result in the closure of the
+// CompletedResearchItem and error channels. However, any errors that occur
+// while processing stdout are streamed to the outbound error channel. The
+// adapter will continue to poll stdout until the pipe is either closed or the
+// parent context is cancelled. If the parent context reports that it is Done,
+// the child process is immediately killed (SIGKILL is sent to the child
+// process).
 func (a *Adapter) Run(ctx context.Context, pendingResearch *contracts.PendingResearchItem) (<-chan *contracts.CompletedResearchItem, <-chan error) {
+	if a.PathResolver == nil {
+		a.PathResolver = DefaultPathResolver
+	}
+
 	completedItemSource := make(chan *contracts.CompletedResearchItem)
 	errorSource := make(chan error)
 	go func() {
 		defer close(completedItemSource)
 		defer close(errorSource)
 
-		path, err := exec.LookPath("analyzer")
+		path, err := a.PathResolver()
 		if err != nil {
 			errorSource <- err
 			return
@@ -71,12 +101,30 @@ func (a *Adapter) Run(ctx context.Context, pendingResearch *contracts.PendingRes
 		}
 
 		scanner := bufio.NewScanner(stdout)
-		go func() {
-			for scanner.Scan() {
+		frameScanner := new(utils.FrameScanner)
+		scanner.Split(frameScanner.ScanFrames)
 
+		for scanner.Scan() {
+			select {
+			case <-ctx.Done():
+				errorSource <- ctx.Err()
+				errorSource <- cmd.Process.Kill()
+				return
+			default:
 			}
-		}()
 
+			if scanner.Err() != nil {
+				errorSource <- scanner.Err()
+			}
+			completedResearchItem := new(contracts.CompletedResearchItem)
+			err = proto.Unmarshal(scanner.Bytes(), completedResearchItem)
+			if err != nil {
+				errorSource <- err
+			}
+			completedItemSource <- completedResearchItem
+		}
+
+		errorSource <- cmd.Wait()
 	}()
 
 	return completedItemSource, errorSource
