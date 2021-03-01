5d853c32a2fe93cd59e87b0730d3ffae70645364
Joe <jecolasurdo@gmail.com>
2021-03-01 14:52:17 -0800

    Defining Engine shape.

--- rust/analyzer/src/manager/mod.rs
+++ rust/analyzer/src/manager/mod.rs
@@ -13,3 +13,22 @@ pub trait HTTPAccessor {
     /// possible, that the resulting byte vector is a complete (not partial) response.
     fn get(&self, uri: &str) -> Result<Vec<u8>,AnalyzerError>;
 }
+
+/// An `AnalysisEngine` represents something that is able to manipulate and analyze audio data.
+pub trait AnalysisEngine {
+
+    /// Takes an byte vector representation of a raw mp3 file, and converts it to raw 16-bit mono
+    /// audio data. If the supplied mp3 data cannot be converted to raw-audio for any reason, an
+    /// `AnalyzerError` is returned.
+    fn mp3_to_raw(&self, mp3: Vec<u8>) -> Result<Vec<i16>, AnalyzerError>;
+    
+
+    /// Takes a 16bit raw audio data and calculates its perceptual hash.  If the method is unable
+    /// to proceed for any reason, it will return an `AnalyzerError`. 
+    fn phash(&self, raw: Vec<i16>) -> Result<Vec<u8>, AnalyzerError>;
+    
+    /// Searches for any likely occurences of `candidate` within `target` and returns the position
+    /// of each occurence in a vector of offsets. Any errors that result during the process of
+    /// finding offsets will immediately return an `AnalyzerError`.
+    fn find_offsets(&self, candidate: Vec<i16>, target: Vec<i16>) -> Result<Vec<i64>, AnalyzerError>;
+}
