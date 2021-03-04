7c82e62505eef37be230c1160559e4ed20a46aa6
Joe <jecolasurdo@gmail.com>
2021-03-03 23:03:47 -0800

    Move frm dynamic dispatch to generics

--- rust/analyzer/src/managers/mod.rs
+++ rust/analyzer/src/managers/mod.rs
@@ -7,12 +7,20 @@ use contracts::{CompletedResearchItem, PendingResearchItem};
 use crossbeam_channel::{unbounded, Receiver};
 use std::thread;
 
-pub struct AnalysisManager {
-    uri_accessor: &'static (dyn FromURI<'static> + Sync),
-    engine: dyn Analyzer + Sync,
+pub struct AnalysisManager<A, U>
+where
+    A: Analyzer + Sync,
+    U: FromURI<'static> + Sync,
+{
+    analyzer: A,
+    uri_accessor: U,
 }
 
-impl AnalysisManager {
+impl<A, U> AnalysisManager<A, U>
+where
+    A: Analyzer + Sync,
+    U: FromURI<'static> + Sync,
+{
     fn run(
         &'static self,
         pending_work_item: &'static PendingResearchItem,
@@ -25,13 +33,13 @@ impl AnalysisManager {
                 .uri_accessor
                 .get(pending_work_item.get_episode().get_media_uri())
                 .unwrap();
-            let episode_raw = self.engine.mp3_to_raw(mp3_data).unwrap();
-            let episode_phash = self.engine.phash(episode_raw).unwrap();
+            let episode_raw = self.analyzer.mp3_to_raw(mp3_data).unwrap();
+            let episode_phash = self.analyzer.phash(episode_raw).unwrap();
             for clip in pending_work_item.get_clips() {
                 mp3_data = self.uri_accessor.get(clip.get_media_uri()).unwrap();
-                let clip_raw = self.engine.mp3_to_raw(mp3_data).unwrap();
-                let clip_phash = self.engine.phash(clip_raw).unwrap();
-                let offsets = self.engine.find_offsets(clip_raw, episode_raw).unwrap();
+                let clip_raw = self.analyzer.mp3_to_raw(mp3_data).unwrap();
+                let clip_phash = self.analyzer.phash(clip_raw).unwrap();
+                let offsets = self.analyzer.find_offsets(clip_raw, episode_raw).unwrap();
 
                 let mut cri = CompletedResearchItem::new();
                 cri.set_lease_id(pending_work_item.get_lease_id().to_string());
