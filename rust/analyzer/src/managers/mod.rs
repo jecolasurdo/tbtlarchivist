196b82ab38fd27d18d7e6468c1528548423fc4e5
Joe <jecolasurdo@gmail.com>
2021-03-05 15:03:51 -0800

    WIP: still trying to come to grips with a decient error strategy

--- rust/analyzer/src/managers/mod.rs
+++ rust/analyzer/src/managers/mod.rs
@@ -3,8 +3,8 @@
 use crate::accessors::FromURI;
 use crate::engines::Analyzer;
 use crate::errors::AnalyzerError;
-use contracts::{CompletedResearchItem, PendingResearchItem};
-use crossbeam_channel::{unbounded, Receiver};
+use contracts::{ClipInfo, CompletedResearchItem, PendingResearchItem};
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::thread;
 
 pub struct AnalysisManager<A, U>
@@ -23,32 +23,51 @@ where
 {
     fn run(
         &'static self,
-        pending_work_item: &'static PendingResearchItem,
+        pri: &'static PendingResearchItem,
     ) -> Receiver<Result<CompletedResearchItem, AnalyzerError>> {
         let (tx, rx) = unbounded();
         thread::spawn(move || {
-            // temporarily unwrapping results. These will be handled
-            // more properly in the future by sending errors to `rx`.
-            let mut mp3_data = self
-                .uri_accessor
-                .get(pending_work_item.get_episode().get_media_uri())
-                .unwrap();
-            let episode_raw = self.analyzer.mp3_to_raw(mp3_data).unwrap();
-            let episode_phash = self.analyzer.phash(episode_raw).unwrap();
-            for clip in pending_work_item.get_clips() {
-                mp3_data = self.uri_accessor.get(clip.get_media_uri()).unwrap();
-                let clip_raw = self.analyzer.mp3_to_raw(mp3_data).unwrap();
-                let clip_phash = self.analyzer.phash(clip_raw).unwrap();
-                let offsets = self.analyzer.find_offsets(clip_raw, episode_raw).unwrap();
-
-                let mut cri = CompletedResearchItem::new();
-                cri.set_lease_id(pending_work_item.get_lease_id().to_string());
-                // finish populating cri
-
-                tx.send(Ok(cri))
-                    .expect("unable to transmit completd researech item");
+            if let Err(err) = self.process_episode(pri, tx) {
+                tx.send(Err(err)).unwrap();
             }
         });
         rx
+    }
+
+    fn process_episode(
+        &'static self,
+        pri: &'static PendingResearchItem,
+        tx: Sender<Result<CompletedResearchItem, AnalyzerError>>,
+    ) -> Result<(), AnalyzerError> {
+        let mut mp3_data = self.uri_accessor.get(pri.get_episode().get_media_uri())?;
+        let episode_raw = self.analyzer.mp3_to_raw(mp3_data)?;
+        let episode_phash = self.analyzer.phash(episode_raw)?;
+        for clip in pri.get_clips() {
+            if let Err(err) = self.process_clip(pri, episode_raw, *clip, tx) {
+                tx.send(Err(err)).unwrap();
+            }
+        }
+        Ok(())
+    }
+
+    fn process_clip(
+        &'static self,
+        pri: &'static PendingResearchItem,
+        episode_raw: Vec<i16>,
+        clip: ClipInfo,
+        tx: Sender<Result<CompletedResearchItem, AnalyzerError>>,
+    ) -> Result<(), AnalyzerError> {
+        let mp3_data = self.uri_accessor.get(clip.get_media_uri())?;
+        let clip_raw = self.analyzer.mp3_to_raw(mp3_data)?;
+        let clip_phash = self.analyzer.phash(clip_raw)?;
+        let offsets = self.analyzer.find_offsets(clip_raw, episode_raw)?;
+
+        let mut cri = CompletedResearchItem::new();
+        cri.set_lease_id(pri.get_lease_id().to_string());
+        // finish populating cri
+
+        tx.send(Ok(cri)).unwrap();
+
+        Ok(())
     }
 }
