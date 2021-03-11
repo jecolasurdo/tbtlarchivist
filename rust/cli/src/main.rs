90c5908f7229238e2a34e55b49538b043e1f9950
Joe <jecolasurdo@gmail.com>
2021-03-11 02:17:30 -0800

    WIP: Trying to tie libraries together via the cli

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,9 +1,21 @@
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-use std::io::Result;
+use analyzer::accessors::http;
+use analyzer::engines::cosine_similarity::{self, Settings};
+use analyzer::managers;
+use anyhow::Result;
 
 fn main() -> Result<()> {
-    println!("Hi. I don't do anything.");
+    let engine_settings = Settings {
+        pass_one_sample_density: 0,
+        pass_one_sample_size: 0,
+        pass_one_threshold: 0.0,
+        pass_two_sample_size: 0,
+        pass_two_threshold: 0.0,
+    };
+    let analyzer_engine = cosine_similarity::new(engine_settings);
+    let uri_accessor = http::Accessor {};
+    let mgr = managers::standard::new(analyzer_engine, uri_accessor);
     Ok(())
 }
