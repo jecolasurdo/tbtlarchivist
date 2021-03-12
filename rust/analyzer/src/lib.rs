4f6ff1ff733dff09a56c05cba78353e16b3a47f4
Joe <jecolasurdo@gmail.com>
2021-03-11 16:53:39 -0800

    turn off lints and main

--- rust/analyzer/src/lib.rs
+++ rust/analyzer/src/lib.rs
@@ -1,24 +1,24 @@
 //! The analyzer provides means for identifying if one audio clip exists within another.
 
-#![warn(
-    missing_docs,
-    missing_doc_code_examples,
-    broken_intra_doc_links,
-    clippy::all,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::as_conversions,
-    clippy::todo,
-    clippy::print_stdout,
-    clippy::use_debug
-)]
-#![allow(
-    clippy::must_use_candidate,
-    clippy::float_cmp,
-    clippy::similar_names,
-    clippy::missing_errors_doc,
-    clippy::missing_const_for_fn
-)]
+// #![warn(
+//     missing_docs,
+//     missing_doc_code_examples,
+//     broken_intra_doc_links,
+//     clippy::all,
+//     clippy::pedantic,
+//     clippy::nursery,
+//     clippy::as_conversions,
+//     clippy::todo,
+//     clippy::print_stdout,
+//     clippy::use_debug
+// )]
+// #![allow(
+//     clippy::must_use_candidate,
+//     clippy::float_cmp,
+//     clippy::similar_names,
+//     clippy::missing_errors_doc,
+//     clippy::missing_const_for_fn
+// )]
 
 pub mod accessors;
 pub mod engines;
--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,21 +1,23 @@
-#[global_allocator]
-static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+// #[global_allocator]
+// static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-use analyzer::accessors::http;
-use analyzer::engines::cosine_similarity::{self, Settings};
-use analyzer::managers;
-use anyhow::Result;
+// use analyzer::accessors::http;
+// use analyzer::engines::cosine_similarity::{self, Settings};
+// use analyzer::managers;
+// use anyhow::Result;
 
-fn main() -> Result<()> {
-    let engine_settings = Settings {
-        pass_one_sample_density: 0,
-        pass_one_sample_size: 0,
-        pass_one_threshold: 0.0,
-        pass_two_sample_size: 0,
-        pass_two_threshold: 0.0,
-    };
-    let analyzer_engine = cosine_similarity::new(engine_settings);
-    let uri_accessor = http::Accessor {};
-    let mgr = managers::standard::new(analyzer_engine, uri_accessor);
-    Ok(())
-}
+// fn main() -> Result<()> {
+//     let engine_settings = Settings {
+//         pass_one_sample_density: 0,
+//         pass_one_sample_size: 0,
+//         pass_one_threshold: 0.0,
+//         pass_two_sample_size: 0,
+//         pass_two_threshold: 0.0,
+//     };
+//     let analyzer_engine = cosine_similarity::new(engine_settings);
+//     let uri_accessor = http::Accessor {};
+//     let mgr = managers::standard::new(analyzer_engine, uri_accessor);
+//     Ok(())
+// }
+
+fn main() {}
