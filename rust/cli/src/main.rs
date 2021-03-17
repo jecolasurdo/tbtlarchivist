52a378ad79a0ea4c2a197f17468f9a03e57260f7
Joe <jecolasurdo@gmail.com>
2021-03-16 22:35:39 -0700

    Initial attempt at running the cli

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,31 +1,44 @@
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-use analyzer::accessors::http;
+use analyzer::accessors::file;
 use analyzer::engines::cosine_similarity::{self, Settings};
 use analyzer::managers;
 use anyhow::Result;
+use protobuf::RepeatedField;
 
 fn main() -> Result<()> {
     let engine_settings = Settings {
-        pass_one_sample_density: 0,
-        pass_one_sample_size: 0,
-        pass_one_threshold: 0.0,
-        pass_two_sample_size: 0,
-        pass_two_threshold: 0.0,
+        pass_one_sample_density: 1,
+        pass_one_sample_size: 9,
+        pass_one_threshold: 0.991,
+        pass_two_sample_size: 50,
+        pass_two_threshold: 0.99,
     };
     let analyzer_engine = cosine_similarity::new(engine_settings);
-    let uri_accessor = http::Accessor {};
+    let uri_accessor = file::Accessor {};
     let mgr = managers::standard::new::<
         analyzer::engines::cosine_similarity::Engine,
-        analyzer::accessors::http::Accessor,
+        analyzer::accessors::file::Accessor,
         analyzer::engines::cosine_similarity::Error,
-        analyzer::accessors::http::Error,
+        analyzer::accessors::file::Error,
         anyhow::Error,
     >(analyzer_engine, uri_accessor);
 
     let ctx = cancel::Token::new();
-    let pri = contracts::PendingResearchItem::default();
+    let mut episode = contracts::EpisodeInfo::default();
+    episode.set_media_uri(String::from(
+        "/Users/Joe/Documents/code/tbtlarchivist/rust/audio/episodes/episode.mp3",
+    ));
+
+    let mut clip = contracts::ClipInfo::default();
+    clip.set_media_uri(String::from(
+        "/Users/Joe/Documents/code/tbtlarchivist/rust/audio/drops/drop.mp3",
+    ));
+    let mut pri = contracts::PendingResearchItem::default();
+    pri.set_lease_id(String::from("test_lease_id"));
+    pri.set_episode(episode);
+    pri.set_clips(RepeatedField::from_vec(vec![clip]));
 
     let rx = mgr.run(&ctx, &pri);
 
